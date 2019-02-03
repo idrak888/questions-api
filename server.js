04ce29f13f5f46954f3ed27f15ba8f5fb65dd6a5
Idrak Mustahsin <playbox8g@gmail.com>
2019-02-03 18:39:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const {question} = require('./db/questions-model');
 const app = express();
 var port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth");
+	if (req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+	}
+	next();
+});
+
+
 app.use(bodyParser.json());
 
 app.get('/questions', (req, res) => {
